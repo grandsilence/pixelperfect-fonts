6ffd39ec34d6a4e19198587924266164ed68a670
Grand Silence <mixtape774@gmail.com>
2019-01-29 20:50:38 +0300

    init

---
+++ Chrome/DevTools/devtools.js
@@ -0,0 +1,39 @@
+chrome.devtools.panels.applyStyleSheet(`
+body, .platform-windows, .monospace, .source-code {
+	font-family: Arial, Tahoma, sans-serif !important;
+}
+
+.tabbed-pane-header-tab {
+	width: auto !important;
+}
+.tabbed-pane-header-tab.selected {
+	background-color: #dedede;
+}
+.tabbed-pane-tab-slider {
+	display: none;
+}
+
+:host-context(.platform-windows) .monospace,
+:host-context(.platform-windows) .source-code,
+.platform-windows .monospace,
+.platform-windows .source-code
+ {
+    font-size: 12px !important;
+    font-family: "Terminus (TTF) for Windows", Consolas, monospace !important;
+}
+
+::shadow :host-context(.platform-windows) .monospace,
+::shadow :host-context(.platform-windows) .source-code,
+::shadow .platform-windows .monospace,
+::shadow .platform-windows .source-code {
+    font-size: 12px !important;
+    font-family: "Terminus (TTF) for Windows", Consolas, monospace !important;
+}
+
+.styles-section.read-only {
+	font-style: normal !important;
+}
+.metrics .label {
+	font-size: 11px !important;
+}
+`);
